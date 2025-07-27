804cc600b47450f54e8c2cc5254e1709c52ddae7
Dima <60436788+XdimonkrasatonX@users.noreply.github.com>
2025-07-28 00:53:52 +0200

    index.php

--- exceltask/index.php
+++ exceltask/index.php
@@ -1,27 +1,19 @@
 <?php
-$filename = 'table.csv'; // Путь к вашему CSV-файлу
-
-// Проверка существования файла
+$filename = 'table.csv';
 if (!file_exists($filename)) {
     die("Файл $filename не найден");
 }
 
-$data = []; // Массив для хранения данных
-
-// Открываем файл в режиме чтения
+$data = [];
 if (($handle = fopen($filename, "r")) !== false) {
-    
-    // Читаем строки CSV
     while (($row = fgetcsv($handle, 1000, ",")) !== false) {
         $data[] = $row;
     }
     
-    fclose($handle); // Закрываем файл
+    fclose($handle);
 } else {
     die("Не удалось открыть файл: $filename");
 }
-
-// Вывод данных (пример)
 echo "<pre>";
 print_r($data);
 echo "</pre>";
